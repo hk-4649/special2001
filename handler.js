f23fe724ecb36a0b94f4c73f4d41c9266e781dd7
hiroshi <princeofneet4@gmail.com>
2021-02-14 03:54:38 +0900

    日の属性を判別する処理追加

--- handler.js
+++ handler.js
@@ -1,10 +1,19 @@
 'use strict';
 const { getPublicHolidays } = require('./API/getPublicHolidays');
-
+const { getTodaysImage } = require('./API/getTodaysImage');
 module.exports.publicHolidays = async (event) => {
   const publicHolidays = await getPublicHolidays();
   return {
     statusCode: 200,
     body: JSON.stringify(publicHolidays),
+  };
+};
+
+module.exports.todaysImage = async (event) => {
+  const { date } = JSON.parse(event.body);
+  const todaysImage = await getTodaysImage(date);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({image: todaysImage}),
   };
 };
